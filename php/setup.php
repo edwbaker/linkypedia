8c25f64610a77b894bb721adcfcc0e028fedd7ba
edwbaker <edwbaker@gmail.com>
2013-02-28 12:23:42 +0000

    Add table for wikipedia pages.

--- php/setup.php
+++ php/setup.php
@@ -15,6 +15,8 @@ ENGINE = InnoDB;";
 
 mysqli_query($db, $sql);
 
+$sql = "CREATE TABLE `linkypedia`.`wiki_data` (`id` INT AUTO_INCREMENT, `wiki_url` TEXT NULL, `wiki_page` LONGTEXT NULL, PRIMARY KEY (`id`) ) ENGINE = InnoDB;"
 
+mysqli_query($db, $sql);
 
 
